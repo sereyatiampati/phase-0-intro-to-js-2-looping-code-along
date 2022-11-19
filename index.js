65699a5ca76581f016b8bc2273b817aa723cd526
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2022-11-19 17:15:40 +0300

    looping using for and while loop

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+//Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event) {
+    event= "surprise";
+    const newArray = [];
+    for(let i = 0; i < names.length; i++)
+    {
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+function countDown (num) {
+    //num=4;
+    const num2=num;
+    let num3=num;
+    let num4=num;
+    while (num >= 0)
+    {
+        console.log( `"Expected countDown(${num2}) to log ${num--}` + 
+        (num3 >= num2 ?  ` first"` : ` after ${num4--} "`));
+        num3--;
+    }
+
+}
+
